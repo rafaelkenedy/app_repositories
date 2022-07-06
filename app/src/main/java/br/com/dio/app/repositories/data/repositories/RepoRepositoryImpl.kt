7d63aea2957809fd4da056062cbdc4cdbf763c81
rafaelkenedy <k.rafaelalves@gmail.com>
2022-07-06 14:03:39 -0300

    Finished!

---
+++ app/src/main/java/br/com/dio/app/repositories/data/repositories/RepoRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package br.com.dio.app.repositories.data.repositories
+
+import br.com.dio.app.repositories.core.RemoteException
+import br.com.dio.app.repositories.data.services.GitHubService
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+
+class RepoRepositoryImpl(private val service: GitHubService) : RepoRepository {
+
+    override suspend fun listRepositories(user: String) = flow {
+        try {
+            val repoList = service.listRepositories(user)
+            emit(repoList)
+
+        } catch (ex: HttpException) {
+            throw RemoteException(ex.message ?: "Não é possível realizar a busca no momento!")
+
+        }
+
+    }
+}
